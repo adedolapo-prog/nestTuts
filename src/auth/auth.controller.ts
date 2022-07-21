b493fc335b063ac895839441b5c289d517d84d5a
adedolapo-prog <shayne.doly94@gmail.com>
2022-07-21 08:11:37 +0100

    sets config

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './Dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  login() {
-    return this.authService.login();
+  login(@Body() auth: AuthDto) {
+    return this.authService.login(auth);
   }
 
   @Post('signup')
-  signup() {
-    return this.authService.signup();
+  signup(@Body() auth: AuthDto) {
+    return this.authService.signup(auth);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './Dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
-  login(): string {
-    return 'I am logged in';
+  constructor(private prisma: PrismaService) {}
+
+  async login(auth: AuthDto): Promise<any> {
+    try {
+      const { email, password } = auth;
+
+      //find user by email
+      const user = await this.prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+      //if no user, throw exception
+      if (!user) throw new ForbiddenException('Credentials incorrect');
+
+      //compare password
+      const pwMatch = await argon.verify(user.hash, password);
+      //if wrong password, throw error
+      if (!pwMatch) throw new ForbiddenException('Credentials incorrect');
+
+      //return user
+      user.hash = undefined;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+
+      throw error;
+    }
   }
-  signup(): string {
-    return 'I have signed up';
+
+  async signup(auth: AuthDto): Promise<any> {
+    try {
+      const { email, password } = auth;
+      //generate our password hash
+      const hash = await argon.hash(password);
+
+      //save new user to the db
+      const user = await this.prisma.user.create({
+        data: {
+          email,
+          hash,
+        },
+      });
+
+      user.hash = undefined;
+
+      //return saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+
+      throw error;
+    }
   }
 }
